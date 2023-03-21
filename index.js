0db323cbf2f1a3e2b7b154dc7a12c5d3d3db4aa9
jimaa maya <jimaa.maya@gmail.com>
2023-03-21 14:33:17 +0300

    phase-0-intro-js-looping-lab-done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+    function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+
+  function writeCards(names) {
+    let arr = [];
+    let i = 0;
+      while (i < names.length) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++
+      }
+      return arr;
+    }
+    
